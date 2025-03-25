59c06ab050710ba8bcb37179975ba401cce9c829
selimyanat <yanat.selim@gmail.com>
2025-03-25 11:52:03 +0100

    fixing build

--- webapp/app/api/url-shortener/route.ts
+++ webapp/app/api/url-shortener/route.ts
@@ -14,23 +14,4 @@ export async function POST(request: NextRequest) {
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 400 });
   }
-  /*
-  return fetch('http://localhost:3000/shorten-url', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ url }),
-  })
-    .then(async (res) => {
-      if (!res.ok)
-        throw new Error((await res.json()).error || 'Failed to shorten URL');
-      return res.json();
-    })
-    .then((data) =>
-      NextResponse.json({ shortenedUrl: data.shortenedUrl }, { status: 200 })
-    )
-    .catch((error) => {
-      console.error('Error shortening URL:', error);
-      return NextResponse.json({ error: error.message }, { status: 400 });
-    });
-    */
 }
