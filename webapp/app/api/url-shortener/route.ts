6e54e135abbb39c7fea6cb960220b059add7e50b
selimyanat <yanat.selim@gmail.com>
2025-03-28 14:53:12 +0100

    dockerize webapp and api

--- webapp/app/api/url-shortener/route.ts
+++ webapp/app/api/url-shortener/route.ts
@@ -2,14 +2,19 @@ import { NextRequest, NextResponse } from 'next/server';
 import { createShortenUrl } from '../../../lib/create-shorten-url';
 
 export async function POST(request: NextRequest) {
-  const { url } = await request.json().catch(() => ({})); // ✅ Simplified JSON parsing
+  const { url } = await request.json().catch(() => ({})); //
 
   if (typeof url !== 'string' || !url.trim()) {
     return NextResponse.json({ error: 'Invalid URL format' }, { status: 400 });
   }
 
   try {
-    const shortenedUrl = await createShortenUrl(url, 'http://localhost:3000');
+    const shortenedBaseUrl = process.env.SHORTENED_BASE_URL;
+
+    if (!shortenedBaseUrl) {
+      throw new Error('SHORTENED_BASE_URL not configured');
+    }
+    const shortenedUrl = await createShortenUrl(url, shortenedBaseUrl);
     return NextResponse.json({ shortenedUrl }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 400 });
